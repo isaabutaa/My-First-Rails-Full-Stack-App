3ddfe5ffef345586131fd14bbd4255fb8e2b932a
isaabutaa <isaabutaa@gmail.com>
2020-03-12 18:54:24 -0400

    Figured out the 422 error. I had to change the permitted params in my users controller file because I changed :names to :first_name and :last_name

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,7 +35,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:name, :image, :title, :position, :direct_reports)
+    params.permit(:first_name, :last_name, :image, :title)
   end
 
   def user
